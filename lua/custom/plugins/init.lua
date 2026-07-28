5af560a8ecc6e5e7283cdd02738a80472894ac88
Rafał Stefanowicz <r.stefanowicz1@gmail.com>
2026-07-28 23:22:45 +0200

    style: format

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,36 +13,33 @@ for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
 end
 
 -- Return to last edit position when opening files
-vim.api.nvim_create_autocmd("BufReadPost", {
+vim.api.nvim_create_autocmd('BufReadPost', {
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
     local line = mark[1]
     local ft = vim.bo.filetype
-    if line > 0 and line <= lcount
-      and vim.fn.index({ "commit", "gitrebase", "xxd" }, ft) == -1
-      and not vim.o.diff then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-      end
+    if line > 0 and line <= lcount and vim.fn.index({ 'commit', 'gitrebase', 'xxd' }, ft) == -1 and not vim.o.diff then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 })
 
-
 do
-  vim.pack.add {  'https://github.com/wansmer/treesj' }
-  require('treesj').setup({ use_default_keymaps = false, max_join_length = 180 })
+  vim.pack.add { 'https://github.com/wansmer/treesj' }
+  require('treesj').setup { use_default_keymaps = false, max_join_length = 180 }
 
   vim.keymap.set('n', '<leader>m', require('treesj').toggle, { desc = 'Split/Join block of code' })
 end
 
 do
   vim.pack.add { 'https://github.com/pmizio/typescript-tools.nvim' }
-  require('typescript-tools').setup({
+  require('typescript-tools').setup {
     expose_as_code_action = 'all',
     tsserver_plugins = { 'styled-components' },
     jsx_close_tag = {
       enable = true,
-      filetypes = { "javascriptreact", "typescriptreact" },
-    }
-  })
+      filetypes = { 'javascriptreact', 'typescriptreact' },
+    },
+  }
 end
