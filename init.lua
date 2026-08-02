a125394ef6c56bb7d211538f80ed0a51bc8d6856
Rafał Stefanowicz <r.stefanowicz1@gmail.com>
2026-08-02 16:58:08 +0200

    feat: remove prettierd and format on save

--- init.lua
+++ init.lua
@@ -775,11 +775,6 @@ do
   vim.pack.add { gh 'stevearc/conform.nvim' }
   require('conform').setup {
     notify_on_error = false,
-    format_on_save = {
-      enabled = true,
-      timeout_ms = 500,
-      lsp_fallback = false,
-    },
     default_format_opts = {
       lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
     },
@@ -790,10 +785,10 @@ do
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      typescript = { 'prettierd', 'prettier', 'deno_fmt', 'biome' },
-      typescriptreact = { 'prettierd', 'prettier', 'deno_fmt', 'biome' },
-      javascript = { 'prettierd', 'prettier', 'deno_fmt', 'biome' },
-      javascriptreact = { 'prettierd', 'prettier', 'deno_fmt', 'biome' },
+      typescript = { 'prettier', 'deno_fmt', 'biome' },
+      typescriptreact = { 'prettier', 'deno_fmt', 'biome' },
+      javascript = { 'prettier', 'deno_fmt', 'biome' },
+      javascriptreact = { 'prettier', 'deno_fmt', 'biome' },
     },
   }
 
